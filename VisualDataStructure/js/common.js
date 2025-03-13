3787bd1efc3ca9367afbbf0ec05071534c8621bc
SRINIVAS DM <srinivasprincea17@gmail.com>
2025-03-13 15:10:43 +0530

    Update common.js

--- VisualDataStructure/js/common.js
+++ VisualDataStructure/js/common.js
@@ -58,39 +58,5 @@ $( document ).ready(function() {
 		$('.overlays').fadeOut(function(){
 			$('#dark-overlay').fadeOut();
 		});
-	});
-	
-	//facebook login stuff
-	/*
-	$('#fb-login').hide();
-	window.fbAsyncInit = function() {
-		FB.init({
-			appId      : '192228707604746', // App ID
-			channelUrl : '//www.rosemarietan.com/fyp/channel.html', // Channel File
-			status     : true, // check login status
-			cookie     : true, // enable cookies to allow the server to access the session
-			xfbml      : true  // parse XFBML
-		});
-	
-		// Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
-		// for any authentication related change, such as login, logout or session refresh. This means that
-		// whenever someone who was previously logged out tries to log in again, the correct case below 
-		// will be handled. 
-		FB.Event.subscribe('auth.authResponseChange', function(response) {
-		// Here we specify what we do with the response anytime this event occurs. 
-			if (response.status === 'connected') {
-				FB.api('/me', function(resp) {
-					$('#fb-login').show();
-					$('#fb-login').html(resp.name);
-					$('#fb-login').attr('href', resp.link);
-					$('#trick').hide();
-				});
-			} else if (response.status === 'not_authorized') {
-				FB.login();
-			} else {
-				FB.login();
-			}
-		});
-	};*/
-	
+	});	
 });
